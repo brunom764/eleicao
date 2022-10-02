223b0213294205cd76e67c9a6e08b2dbecd141d0
Bruno Miguel Moreira Albuquerque <100159869+brunom764@users.noreply.github.com>
2022-10-02 18:05:28 -0300

    Update eleicao.py

--- eleicao.py
+++ eleicao.py
@@ -4,23 +4,20 @@ import pandas as pd
 
 URL = requests.get(
     'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json')
-
-json_URL = json.loads(URL.content)
+jsonURL = json.loads(URL.content)
 
 candidato = []
 partido = []
 votos = []
 porcentagem = []
 
-for info in json_URL['cand']:
-
+for info in jsonURL['cand']:
     if info['seq'] == '1' or info['seq'] == '2' or info['seq'] == '3' or info['seq'] == '4':
         candidato.append(info['nm'])
         votos.append(info['vap'])
         porcentagem.append(info['pvap'])
 
-df_eleicao = pd.DataFrame(list(zip(candidato, votos, porcentagem)), columns=[
-    'Candidato', 'Nº de Votos', 'Porcentagem'
-])
+eleicao = pd.DataFrame(list(zip(candidato, votos, porcentagem)), columns=[
+    'Candidato', 'Nº de Votos', 'Porcentagem'])
 
-print(df_eleicao)
+print(eleicao)
